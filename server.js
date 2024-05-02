2be6daed6495caf37227b6dabcba27b4e027d228
UltimaVanguard <ultimavanguard@gmail.com>
2024-05-02 11:00:54 -0500

    installed express, moved CLI functionality to server.js, and added basic functionality to view all employees, departments, and roles

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const inquirer = require('inquirer');
 const express = require('express')
 const { Pool } = require('pg');
 const CLI = require('./lib/cli.js');
@@ -28,6 +29,87 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
-const cli = new CLI();
+// const cli = new CLI();
 
-cli.run();
+// cli.run();
+
+function viewEmployees() {
+    pool.query('SELECT * FROM employee ORDER BY first_name, last_name', function(err, {rows}) {
+        console.log(rows)
+    })
+    return run();
+};
+
+function addEmployee() {
+    return run();
+};
+
+function updateRole() {
+    return run();
+};
+
+function viewRoles() {
+    pool.query('SELECT * FROM role ORDER BY title', function(err, {rows}) {
+        console.log(rows)
+    })
+    return run();
+};
+
+function addRole() {
+    return run();
+};
+
+function viewDepartments() {
+    pool.query('SELECT * FROM department ORDER BY name', async function(err, {rows}) {
+        console.log(rows)
+        return run();
+    })
+};
+
+function addDepartment() {
+    return run();
+};
+
+function run() {
+    return inquirer
+        .prompt([
+        {
+            type: 'list',
+            name: 'todo',
+            message: 'What would you like to do?',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role',
+                    'View All Roles', 'Add Role', 'View All Departments', 'Add Department',
+                    'Quit'],
+            }
+        ])
+        .then(({ todo }) => {
+            switch (todo) {
+                case 'View All Employees':
+                    viewEmployees();
+                    break;
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+                case 'Update Employee Role':
+                    updateRole();
+                    break;
+                case 'View All Roles':
+                    viewRoles();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
+                case 'View All Departments':
+                        viewDepartments();
+                        break;
+                case 'Add Department':
+                    addDepartment();
+                    break;
+                default:
+                    return;
+                    break;
+                };
+            });
+}
+
+run();
