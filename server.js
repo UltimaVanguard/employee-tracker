b7f5b9735c47f2e6fd1f4d2bc58f8ccc517af2df
UltimaVanguard <ultimavanguard@gmail.com>
2024-05-03 15:23:05 -0500

    added addRole and addEmployee Functionality

--- server.js
+++ server.js
@@ -37,49 +37,63 @@ app.listen(PORT, () => {
 
 // cli.run();
 
-
-function roleChoices() {
-    pool.query('SELECT title AS name FROM role, id AS value', async function(err, {rows}) {
-        roles = rows
-        await console.log(roles)
-    })
-    return roles;
-};
-
 function viewEmployees() {
     pool.query(`SELECT employee.id, CONCAT(employee.first_name, ' ', employee.last_name) AS emp_name, role.title, role.salary, department.name, ` +
                `CONCAT(manager.first_name, ' ', manager.last_name) AS manager ` +
                'FROM employee employee LEFT JOIN employee manager ON employee.manager_id = manager.id ' +
-               'INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id' , async function(err, { rows }) {
+               'INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id ' +
+               'ORDER BY emp_name', async function(err, { rows }) {
                 await console.table(rows)
                 return run();
         })
 };
 
 function addEmployee() {
-    inquirer
-        .prompt([
-        {
-            type: 'input',
-            name: 'firstName',
-            message: `What is the employee's first name?`,
-        },
-        {
-            type: 'input',
-            name: 'lastName',
-            message: `What is the employee's last name?`,
-        },
-        {
-            type: 'list',
-            name: 'role',
-            message: `What is the employee's role?`,
-            choices: roleChoices(),
-        },
-    ])
-    .then(({ firstName, lastName, role, department }) => {
-
+    pool.query('SELECT title AS name, id AS value FROM role', async function(err, {rows}) {
+        const roles = rows
+        await pool.query(`SELECT CONCAT(first_name, ' ', last_name) AS name,` +
+                         'id AS value FROM employee WHERE role_id IN (1,2,3)', async function(err, {rows}) {
+            const managers = rows
+            await inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'firstName',
+                        message: `What is the employee's first name?`,
+                    },
+                    {
+                        type: 'input',
+                        name: 'lastName',
+                        message: `What is the employee's last name?`,
+                    },
+                    {
+                        type: 'list',
+                        name: 'role',
+                        message: `What is the employee's role?`,
+                        choices: roles,
+                    },
+                    {
+                        type: 'list',
+                        name: 'manager',
+                        message: `Who is the employee's manager?`,
+                        choices: managers,
+                    },
+                ])
+                .then(({ firstName, lastName, role, manager }) => {
+                    pool.query('INSERT INTO employee (first_name, last_name, role_id, manager_id)' +
+                               'VALUES ($1, $2, $3, $4)', [firstName, lastName, role, manager], async (err) => {
+                        if (err) {
+                            await console.log(err.message);
+                            return
+                        } else {
+                            await console.log('');
+                            await console.log(`${firstName} ${lastName} successfully added!`);
+                            return run();
+                        }
+                    });
+                })
+        })
     })
-    return run();
 };
 
 function updateRole() {
@@ -95,7 +109,41 @@ function viewRoles() {
 };
 
 function addRole() {
-    return run();
+    pool.query('SELECT name, id AS value FROM department', async function(err, {rows}) {
+        const departments = rows
+        await inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'title',
+                    message: `What is the title of the role?`,
+                },
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: `What is the salary for the role?`,
+                },
+                {
+                    type: 'list',
+                    name: 'department',
+                    message: `What department is the role under?`,
+                    choices: departments,
+                },
+            ])
+            .then(({ title, salary, department }) => {
+                pool.query('INSERT INTO role (title, salary, department_id)' +
+                           'VALUES ($1, $2, $3)', [title, salary, department], async (err) => {
+                    if (err) {
+                        await console.log(err.message);
+                        return
+                    } else {
+                        await console.log('');
+                        await console.log(`${title} successfully added!`);
+                        return run();
+                    }
+                });
+            })
+    })
 };
 
 function viewDepartments() {
@@ -121,7 +169,7 @@ function addDepartment() {
                 return
             } else {
                 await console.log('');
-                await console.log(`${department} sucessfully added!`);
+                await console.log(`${department} successfully added!`);
                 return run();
             }
         }
